2a0bde62c1532bb750993f56af02662a4a49fad7
Shun Kakinoki <shunkakinoki@gmail.com>
2020-03-29 15:14:07 +0900

    style: 💄 fix naming

--- lib/app/routers/routes.dart
+++ lib/app/routers/routes.dart
@@ -29,7 +29,7 @@ class Routes {
         handler: Handler(
             handlerFunc:
                 (BuildContext context, Map<String, List<String>> params) =>
-                    MyHomePage()));
+                    HomePage()));
 
     _listRouter.clear();
 

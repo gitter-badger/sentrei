8e228d4dbb0ef423a852d0317eb2e826ad8afd62
Shun Kakinoki <shunkakinoki@gmail.com>
2020-03-29 15:10:51 +0900

    feat: 🎸 ini provider fluro
    initiate provider package with fluro routing

---
+++ lib/app/routers/routes.dart
@@ -0,0 +1,40 @@
+/// References:
+/// https://github.com/simplezhli/flutter_deer/blob/master/lib/routers/routers.dart
+/// https://github.com/simplezhli/flutter_deer/blob/master/lib/routers/router_init.dart
+
+import 'package:flutter/material.dart';
+
+import 'package:fluro/fluro.dart';
+
+import 'package:sentrei/home/home.dart';
+import '404.dart';
+
+abstract class IRouterProvider {
+  void initRouter(Router router);
+}
+
+class Routes {
+  static String home = '/home';
+
+  static List<IRouterProvider> _listRouter = [];
+
+  static void configureRoutes(Router router) {
+    router.notFoundHandler = Handler(
+        handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+      debugPrint('Route not found');
+      return WidgetNotFound();
+    });
+
+    router.define(home,
+        handler: Handler(
+            handlerFunc:
+                (BuildContext context, Map<String, List<String>> params) =>
+                    MyHomePage()));
+
+    _listRouter.clear();
+
+    _listRouter.forEach((routerProvider) {
+      routerProvider.initRouter(router);
+    });
+  }
+}

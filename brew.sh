afb8adeeace5cc9a3d200292c2df6875db6f786f
Shun Kakinoki <shunkakinoki@gmail.com>
2020-03-29 12:41:08 +0900

    fix: 🐛 add brew cache

---
+++ brew.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+brew install imagemagick
+brew install graphicsmagick
+brew install librsvg
+brew unlink pango
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/7cf3b63be191cb2ce4cd86f4406915128ec97432/Formula/pango.rb
+brew switch pango 1.42.4_1

4204c0f2176cc0e59e5d7b5c7f289f05428329c3
Shun Kakinoki <shunkakinoki@gmail.com>
2020-04-02 18:10:17 +0900

    fix: 🐛 add $ANDROID_SIGN_PWD

--- sentrei/android/key.sh
+++ sentrei/android/key.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_SIGN_PWD" \
---output firebase/api.json firebase/api.json.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_SIGN_PWD" \
---output firebase/key.jks firebase/key.jks.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_SIGN_PWD" \
 --output firebase/sentrei-alpha.json firebase/sentrei-alpha.json.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_SIGN_PWD" \
 --output firebase/sentrei-beta.json firebase/sentrei-beta.json.gpg
@@ -26,8 +22,6 @@ gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_SIGN_PWD" \
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_SIGN_PWD" \
 --output key/sentrei-master.json key/sentrei-master.json.gpg
 
-rm -f key.properties
 echo "storePassword=$ANDROID_SIGN_PWD" >> key.properties
 echo "keyPassword=$ANDROID_SIGN_PWD" >> key.properties
 echo "keyAlias=key" >> key.properties
-echo "storeFile=key/key.jks" >> key.properties
